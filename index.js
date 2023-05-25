4c56b6c3ec51e05f9bf186ae0cb04ac88bad7d6a
MikeSusutZZ <121924073+MikeSusutZZ@users.noreply.github.com>
2023-05-24 21:56:51 -0700

    Bugfixed if target isn't selected

--- index.js
+++ index.js
@@ -599,14 +599,16 @@ var summonerNames = [dbRet[0].player1, dbRet[0].player2, dbRet[0].player3, dbRet
 
 app.post("/update", async (req, res) => {
   input = req.body.champName;
-  if(input == null){
+  target = req.query.tar;
+  if(input == null || target == ""){
+    console.log("empty");
     res.redirect("/teamView");
     return;
   }
   console.log(input);
   await teamsCollection.updateOne(
     { code: req.session.teamCode },
-    { $set: { [req.query.tar]: input } }
+    { $set: { [target]: input } }
   );
 
   res.redirect("/teamView");
@@ -614,7 +616,8 @@ app.post("/update", async (req, res) => {
 
 app.post("/updatePicks", async (req, res) => {
   input = req.body.champName;
-  if(input == null){
+  target = req.query.tar;
+  if(input == null || target == ""){
     res.redirect("/picks");
     return;
   }
@@ -623,7 +626,7 @@ app.post("/updatePicks", async (req, res) => {
   if(!req.session.guest){
   await userCollection.updateOne(
     { email: req.session.email },
-    { $set: { [req.query.tar]: input } }
+    { $set: { [target]: input } }
   );}
   if(req.session.teamCode != null){
     res.redirect("/teamView")
